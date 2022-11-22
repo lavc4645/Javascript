db761d5ac72ebd7df3ed49c006991e47cb7f8ef7
lavc4645 <lavc4645@gmail.com>
2022-11-22 11:29:37 +0530

    First commit

---
+++ TwoSum.js
@@ -0,0 +1,28 @@
+/** Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1]. */
+const checkSum = () => {
+  const r1 = require("readline-sync");
+  let nums = [];
+  let sum;
+  let i = r1.question("Enter the size of array ");
+  for (let index = 0; index < i; index++) {
+    nums.push(r1.prompt());
+    console.log(nums);
+  }
+  let target = r1.question("Enter the target value ");
+  let NumIndex = [2];
+  for (let index = 0; index + 1 < nums.length; index++) {
+    sum = nums[index] + nums[index + 1];
+    NumIndex[0] = index;
+    NumIndex[1] = index + 1;
+    if (sum == target) {
+      console.log("Num Index", NumIndex);
+      return NumIndex;
+    }
+  }
+  console.log("No solution");
+};
+const answer = checkSum();
+console.log(answer);
